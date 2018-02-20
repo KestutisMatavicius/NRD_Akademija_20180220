91a1039c064cc060b0e8495689d72ec3a98f386d
kma <kma@nrd.lt>
2018-02-20 19:35:38 +0200

    Sutvarkome jungimąsi prei db

--- akademija.EF/NrdAkademijaDbContext.cs
+++ akademija.EF/NrdAkademijaDbContext.cs
@@ -5,21 +5,16 @@ namespace akademija.EF
 {
     public partial class NrdAkademijaDbContext : DbContext
     {
+        public NrdAkademijaDbContext(DbContextOptions<NrdAkademijaDbContext> options)
+    : base(options)
+        { }
+
         public virtual DbSet<Blog> Blog { get; set; }
         public virtual DbSet<Employee> Employee { get; set; }
         public virtual DbSet<EmployeeInventory> EmployeeInventory { get; set; }
         public virtual DbSet<Inventory> Inventory { get; set; }
         public virtual DbSet<InventoryType> InventoryType { get; set; }
         public virtual DbSet<Post> Post { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer(@"Server=(localdb)\ProjectsV12;Database=NrdAkademijaDb;Trusted_Connection=True;");
-            }
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
