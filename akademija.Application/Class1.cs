7b4cac892581c9d45680ce7b3b38140df2210d25
kma <kma@nrd.lt>
2018-02-20 17:57:35 +0200

    Prideti nugetai į EF projektą

---
+++ akademija.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace akademija.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ akademija.EF/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace akademija.EF
+{
+    public class Class1
+    {
+    }
+}
